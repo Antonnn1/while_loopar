837b4b81686add8a00c1b11d18ce669cac79c3d3
antonnn1 <antonnn@vgy.se>
2016-09-23 14:48:51 +0200

    ovn3

--- While_loopar/Program.cs
+++ While_loopar/Program.cs
@@ -18,18 +18,26 @@ namespace While_loopar
                  Console.WriteLine(i + " ");
              }
              */
-
-            //Övn 2
-            string inmatat = "";
-            while (inmatat != "develooper")
+            /*
+           //Övn 2
+           string inmatat = "";
+           while (inmatat != "develooper")
+           {
+             Console.Write("Skriv in lösenord: ");
+             inmatat = Console.ReadLine();
+             if (inmatat == "develooper")
+                 Console.WriteLine("Korrekt lösenord");
+             else
+                 Console.WriteLine("Inkorrekt lösenord");
+           }
+           */
+            //Övn 3
+            int i = 1;
+            while (i++<=100)
             {
-                Console.Write("Skriv in lösenord: ");
-                inmatat = Console.ReadLine();
-                if (inmatat == "develooper")
-                    Console.WriteLine("Korrekt lösenord");
-                else
-                    Console.WriteLine("Inkorrekt lösenord");
+                Console.WriteLine(i);
             }
+            Console.ReadLine();
         }
     }
 }
